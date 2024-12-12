eb86790cf0e8922199ff866a23e8c154c0434836
thomasdubost <thomas.dubost@etu.u-paris.fr>
2024-12-12 18:02:25 +0100

    Create chevalet.h

---
+++ chevalet.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int lettre_max = 21;
+typedef struct chevalet {
+  lettre[]={A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V}
+  quantite[]={9,1,2,3,14,1,1,1,7,1,5,3,6,5,2,1,6,7,6,5,2}
+}chevalet;
+char nouveau_chevalet(lettre * l, quantite * q);
